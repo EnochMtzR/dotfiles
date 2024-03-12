915eb267cd9d444d0f147e0290428facf491593f
JosnocSackra <josnocpp@gmail.com>
2024-03-12 11:17:37 -0600

    Fixed color issues on tmux and lualine separators.

--- .config/nvim/lua/sackra/plugins/lualine.lua
+++ .config/nvim/lua/sackra/plugins/lualine.lua
@@ -5,6 +5,10 @@ return {
         local lualine = require('lualine')
 
         lualine.setup {
+            options = {
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '|', right = '|' },
+            },
         }
     end
 }
