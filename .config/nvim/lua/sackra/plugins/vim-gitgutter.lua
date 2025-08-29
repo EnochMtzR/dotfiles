41cc54d432498a1009ceddd986510c18371abbce
JosnocSackra <josnocpp@gmail.com>
2025-08-29 15:16:59 -0600

    Added nextHunk and prevHunk keymaps for gitGutter.

--- .config/nvim/lua/sackra/plugins/vim-gitgutter.lua
+++ .config/nvim/lua/sackra/plugins/vim-gitgutter.lua
@@ -3,8 +3,10 @@ return {
     config = function()
         vim.cmd("GitGutterLineNrHighlightsToggle")
 
-        vim.keymap.set('n', 'gn', ':GitGutterLineNrHighlightsToggle<CR>',
+        vim.keymap.set('n', 'gl', ':GitGutterLineNrHighlightsToggle<CR>',
             { desc = "Toggle gitgutter line number highlights" })
         vim.keymap.set('n', 'gh', ':GitGutterLineHighlightsToggle<CR>', { desc = "Toggle gitgutter line highlights" })
+        vim.keymap.set('n', 'gn', ':GitGutterNextHunk<CR>', { desc = "Go to next git hunk" })
+        vim.keymap.set('n', 'gp', ':GitGutterPrevHunk<CR>', { desc = "Go to previous git hunk" })
     end,
 }
